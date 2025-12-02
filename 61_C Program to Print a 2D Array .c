2f657a0d8a361efaa71305c07a73e386ed99efb1
mdobydul <mdobydul7977@gmail.com>
2025-12-03 01:12:07 +0600

    Create 61_C Program to Print a 2D Array .c

---
+++ 61_C Program to Print a 2D Array .c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int arr[rows][cols];
+
+    // Input elements
+    printf("Enter elements of the array:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    // Print array
+    printf("The 2D array is:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
